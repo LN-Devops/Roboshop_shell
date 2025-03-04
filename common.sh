c4c973b6da5ea6cfaf5c66579fdbe83e3e0fe4c4
Lakshman Nethi <nlntr225@gmail.com>
2025-03-04 16:08:28 -0600

    frontend of Roboshop

--- common.sh
+++ common.sh
@@ -5,19 +5,20 @@ SYSTEMD_SETUP(){
   systemctl enable $app_name
   systemctl start $app_name
 }
-
-NODEJS(){
-  dnf module disable nodejs -y
-  dnf module enable nodejs:20 -y
-  dnf install nodejs -y
+APP_PREREQ(){
   useradd roboshop
   rm -rf /app
   mkdir /app
   cd /app
   curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
   unzip /tmp/$app_name.zip
-
   cd /app
+}
+NODEJS(){
+  dnf module disable nodejs -y
+  dnf module enable nodejs:20 -y
+  dnf install nodejs -y
+  APP_PREREQ
   npm install
   SYSTEMD_SETUP
 
@@ -26,16 +27,7 @@ NODEJS(){
 JAVA(){
   dnf install maven -y
 
-  useradd roboshop
-
-  rm -rf /app
-  mkdir /app
-
-  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
-  cd /app
-  unzip /tmp/$app_name.zip
-
-  cd /app
+  APP_PREREQ
   mvn clean package
   mv target/$app_name-1.0.jar $app_name.jar
   SYSTEMD_SETUP
@@ -43,13 +35,7 @@ JAVA(){
 
 python(){
   dnf install python3 gcc python3-devel -y
-  useradd roboshop
-  rm -rf /app
-  mkdir /app
-  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
-  cd /app
-  unzip /tmp/$app_name.zip
-  cd /app
+  APP_PREREQ
   pip3 install -r requirements.txt
   SYSTEMD_SETUP
 }
