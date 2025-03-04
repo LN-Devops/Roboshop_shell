843547e48b45c2ff6eeb6a1994429ad0428e7aa2
Lakshman Nethi <nlntr225@gmail.com>
2025-03-04 15:16:40 -0600

    frontend of Roboshop

--- user.sh
+++ user.sh
@@ -1,10 +1,6 @@
 source ./common.sh
 app_name=user
 
-cp $app_name.service /etc/systemd/system/$app_name.service
+
 NODEJS
 
-
-systemctl daemon-reload
-systemctl enable $app_name
-systemctl start $app_name
