18dc66f32d99249168fbd36febe034203bf43b2b
Lakshman Nethi <nlntr225@gmail.com>
2025-03-04 15:54:07 -0600

    frontend of Roboshop

--- common.sh
+++ common.sh
@@ -18,3 +18,23 @@ NODEJS(){
   systemctl enable $app_name
   systemctl start $app_name
 }
+
+JAVA(){
+  dnf install maven -y
+  cp $app_name.service /etc/systemd/system/$app_name.service
+  useradd roboshop
+
+  rm -rf /app
+  mkdir /app
+
+  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
+  cd /app
+  unzip /tmp/$app_name.zip
+
+  cd /app
+  mvn clean package
+  mv target/$app_name-1.0.jar $app_name.jar
+  systemctl daemon-reload
+  systemctl enable $app_name
+  systemctl start $app_name
+}
