f52638ea6a0d748db85ea06f868eefe8e43799d6
Lakshman Nethi <nlntr225@gmail.com>
2025-03-05 14:17:37 -0600

    frontend of Roboshop

--- common.sh
+++ common.sh
@@ -1,59 +1,60 @@
 dir_path=$(pwd)
 log_file=/tmp/roboshop.log
+rm -rf $log_file
 
 
 SYSTEMD_SETUP(){
   echo Creating service for the application
-  cp $dir_path/$app_name.service /etc/systemd/system/$app_name.service >$log_file
+  cp $dir_path/$app_name.service /etc/systemd/system/$app_name.service $>>$log_file
   echo re-loading the service
-  systemctl daemon-reload >$log_file
+  systemctl daemon-reload $>>$log_file
   echo enabling the service
-  systemctl enable $app_name >$log_file
+  systemctl enable $app_name $>>$log_file
   echo starting the service
-  systemctl start $app_name >$log_file
+  systemctl start $app_name $>>$log_file
 }
 APP_PREREQ(){
   echo adding user to the application
-  useradd roboshop >$log_file
+  useradd roboshop $>>$log_file
   echo removing the directory
-  rm -rf /app >$log_file
+  rm -rf /app $>>$log_file
   echo creating the directory
-  mkdir /app >$log_file
-  cd /app >$log_file
+  mkdir /app $>>$log_file
+  cd /app $>>$log_file
   echo downloading the artifactory for application
-  curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip >$log_file
+  curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip $>>$log_file
   echo extracting the artifacts
-  unzip /tmp/$app_name.zip >$log_file
-  cd /app >$log_file
+  unzip /tmp/$app_name.zip $>>$log_file
+  cd /app $>>$log_file
 }
 NODEJS(){
   echo disabling the nodejs version
-  dnf module disable nodejs -y >$log_file
+  dnf module disable nodejs -y $>>$log_file
   echo enabling the nodejs version
-  dnf module enable nodejs:20 -y >$log_file
+  dnf module enable nodejs:20 -y $>>$log_file
   echo installing the nodejs version
-  dnf install nodejs -y >$log_file
+  dnf install nodejs -y $>>$log_file
   APP_PREREQ
   echo downloading the required dependancies
-  npm install >$log_file
+  npm install $>>$log_file
   SYSTEMD_SETUP
 
 }
 
 JAVA(){
   echo downloading maven for java application
-  dnf install maven -y  >$log_file
+  dnf install maven -y  $>>$log_file
 
   APP_PREREQ
   echo downloading the dependancies
-  mvn clean package >$log_file
-  mv target/$app_name-1.0.jar $app_name.jar >$log_file
+  mvn clean package $>>$log_file
+  mv target/$app_name-1.0.jar $app_name.jar $>>$log_file
   SYSTEMD_SETUP
 }
 
 python(){
-  dnf install python3 gcc python3-devel -y >$log_file
+  dnf install python3 gcc python3-devel -y $>>$log_file
   APP_PREREQ
-  pip3 install -r requirements.txt >$log_file
+  pip3 install -r requirements.txt $>>$log_file
   SYSTEMD_SETUP
 }
