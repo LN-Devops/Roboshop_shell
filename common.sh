235b2356cf35580a8ffcd8d48ed208e00585b53e
Lakshman Nethi <nlntr225@gmail.com>
2025-03-05 14:11:34 -0600

    frontend of Roboshop

--- common.sh
+++ common.sh
@@ -1,41 +1,59 @@
 dir_path=$(pwd)
+log_file=/tmp/roboshop.log
+
+
 SYSTEMD_SETUP(){
-  cp $dir_path/$app_name.service /etc/systemd/system/$app_name.service
-  systemctl daemon-reload
-  systemctl enable $app_name
-  systemctl start $app_name
+  echo Creating service for the application
+  cp $dir_path/$app_name.service /etc/systemd/system/$app_name.service >$log_file
+  echo re-loading the service
+  systemctl daemon-reload >$log_file
+  echo enabling the service
+  systemctl enable $app_name >$log_file
+  echo starting the service
+  systemctl start $app_name >$log_file
 }
 APP_PREREQ(){
-  useradd roboshop
-  rm -rf /app
-  mkdir /app
-  cd /app
-  curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
-  unzip /tmp/$app_name.zip
-  cd /app
+  echo adding user to the application
+  useradd roboshop >$log_file
+  echo removing the directory
+  rm -rf /app >$log_file
+  echo creating the directory
+  mkdir /app >$log_file
+  cd /app >$log_file
+  echo downloading the artifactory for application
+  curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip >$log_file
+  echo extracting the artifacts
+  unzip /tmp/$app_name.zip >$log_file
+  cd /app >$log_file
 }
 NODEJS(){
-  dnf module disable nodejs -y
-  dnf module enable nodejs:20 -y
-  dnf install nodejs -y
+  echo disabling the nodejs version
+  dnf module disable nodejs -y >$log_file
+  echo enabling the nodejs version
+  dnf module enable nodejs:20 -y >$log_file
+  echo installing the nodejs version
+  dnf install nodejs -y >$log_file
   APP_PREREQ
-  npm install
+  echo downloading the required dependancies
+  npm install >$log_file
   SYSTEMD_SETUP
 
 }
 
 JAVA(){
+  echo downloading maven for java application
   dnf install maven -y
 
   APP_PREREQ
-  mvn clean package
-  mv target/$app_name-1.0.jar $app_name.jar
+  echo downloading the dependancies
+  mvn clean package >$log_file
+  mv target/$app_name-1.0.jar $app_name.jar >$log_file
   SYSTEMD_SETUP
 }
 
 python(){
-  dnf install python3 gcc python3-devel -y
+  dnf install python3 gcc python3-devel -y >$log_file
   APP_PREREQ
-  pip3 install -r requirements.txt
+  pip3 install -r requirements.txt >$log_file
   SYSTEMD_SETUP
 }
